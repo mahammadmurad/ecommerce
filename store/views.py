e948867c0ae4fcb4d1d32824dfa37fca06ceceea
mahammadmurad <mahammad.muradd@gmail.com>
2023-12-12 17:25:22 +0400

    rating avarage and counting created

--- store/views.py
+++ store/views.py
@@ -7,6 +7,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Q
 from django.contrib import messages
 from .forms import ReviewForm
+from orders.models import OrderProduct
 
 def store(request, category_slug=None):
     categories = None
@@ -43,9 +44,22 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
     
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
+
+    # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+    
     context = {
         'single_product': single_product,
-        'in_cart':in_cart
+        'in_cart':in_cart,
+        'orderproduct': orderproduct,
+        'reviews': reviews,
     }
     
     return render(request, 'store/product_detail.html', context)
