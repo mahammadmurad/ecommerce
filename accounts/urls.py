a6aad3b878f7af3e054a9c836ece6e6e1d10ebe0
mahammadmurad <mahammad.muradd@gmail.com>
2023-12-12 18:16:54 +0400

    dashboard sidebar created

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,9 +7,12 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    
     path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.dashboard, name='dashboard'),
     path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
     path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
     path('resetPassword/', views.resetPassword, name='resetPassword'),
+
+    path('my_orders/', views.my_orders, name='my_orders'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -14,6 +14,7 @@ from django.core.mail import EmailMessage
 
 from carts.views import _cart_id
 from carts.models import *
+from orders.models import Order
 
 import requests
 
@@ -143,15 +144,15 @@ def activate(request, uidb64, token):
 
 @login_required(login_url = 'login')
 def dashboard(request):
-    # orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
-    # orders_count = orders.count()
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
 
     # userprofile = UserProfile.objects.get(user_id=request.user.id)
-    # context = {
-    #     'orders_count': orders_count,
-    #     'userprofile': userprofile,
-    # }
-    return render(request, 'accounts/dashboard.html') #context)
+    context = {
+        'orders_count': orders_count,
+        #'userprofile': userprofile,
+    }
+    return render(request, 'accounts/dashboard.html' ,context)
 
 
 def forgotPassword(request):
@@ -214,3 +215,12 @@ def resetPassword(request):
             return redirect('resetPassword')
     else:
         return render(request, 'accounts/resetPassword.html')
+    
+
+@login_required(login_url='login')
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+    context = {
+        'orders': orders,
+    }
+    return render(request, 'accounts/my_orders.html', context)
