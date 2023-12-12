aef88f1e9af82e196ce7dc98d5676a86c8b0b507
mahammadmurad <mahammad.muradd@gmail.com>
2023-12-12 23:08:38 +0400

    product gallery created

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from .models import *
 
+import admin_thumbnails
+
+
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_avialable')
     prepopulated_fields = {'slug':('product_name',)}
+    inlines = [ProductGalleryInline]
+
 
 @admin.register(Variation)
 class VariationAdmin(admin.ModelAdmin):
@@ -12,6 +22,10 @@ class VariationAdmin(admin.ModelAdmin):
     list_editable = ('is_active',)
     list_filter = ('product', 'variation_category', 'variation_value', 'is_active')
 
+
+
+
 admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
 
 
