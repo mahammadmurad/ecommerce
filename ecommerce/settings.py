3709829d2edc1cc0551c178cc927146318073a2b
mahammadmurad <mahammad.muradd@gmail.com>
2023-12-13 12:10:37 +0400

    connected with postgreSQL

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -94,8 +94,12 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
